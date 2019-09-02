c44c989f20f408f9b937552ba3f60da813f5e738
Neeemoo <33062961+Neeemoo@users.noreply.github.com>
2019-09-02 21:16:28 +0200

    progress - 190902-21.16
    added a date validation function (a lot of if / else if);

--- write_file.py
+++ write_file.py
@@ -3,19 +3,54 @@ import os
 
 class WriteFile:
     file_location = 'C:/Users/' + os.getlogin() + '/.PyHourRecorder'
-    time1 = "00:00"
-    time2 = "00:00"
-    time3 = "00:00"
-    time4 = "00:00"
-    Day = "Mon"
-    Date = "00.00.0000"
+
+    time_work_start = []
+    time_lunch_start = []
+    time_lunch_end = []
+    time_work_end = []
+    Day_of_week = ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"]
+    Date_dd_mm_yyyy = [dd.mm.yyyy, dd + 1, mm, yyyy, dd + 2, mm, yyyy, dd + 3, mm, yyyy, dd + 4, mm, yyyy, dd + 5, mm, yyyy, dd + 6, mm, yyyy, dd + 7, mm, yyyy, dd + 8, mm, yyyy, dd + 9, mm, yyyy, dd + 10, mm, yyyy]
 
     first_file_print = "+-----------------------------------+ \n" \
-                       "| Start:  | " + Day + ", " + Date + " | " + time1 + " | \n" \
-                       "| Mittag: | --------------- | " + time2 + " | \n" \
-                       "| M-Ende: | --------------- | " + time3 + " | \n" \
-                       "| Ende:   | --------------- | " + time4 + " | \n" \
-                       "+-----------------------------------+"
+                       "| Start:  | " + Day[0] + ", " + Date[0] + " | " + time_work_start[0] + " | \n" \
+                       "| Mittag: | --------------- | " + time_lunch_start[0] + " | \n" \
+                       "| M-Ende: | --------------- | " + time_lunch_ende[0] + " | \n" \
+                       "| Ende:   | --------------- | " + time_work_ende[0] + " | \n" \
+                       "+-----------------------------------+ "
+
+    def validate_date(self):
+        yyyy = 1111
+        mm = 1
+        dd = 1
+        if mm == 01 or 03 or 05 or 07:
+            if dd > 31:
+                mm += 1
+                dd - 31
+
+        elif mm == 8 or 10 or 12:
+            if dd > 31:
+                mm += 1
+                dd - 31
+
+            if mm == 12:
+                if dd > 31:
+                    mm = 01
+                    dd - 31
+
+        elif mm == 4 or 6 or 9 or 11:
+            if dd > 30:
+                mm += 1
+                dd - 30
+
+        elif mm == 02:
+            if dd > 29 and yyyy == isinstance(yyyy / 4, int):
+                mm += 1
+                dd - 29
+
+            if dd > 28 and yyyy == isinstance(yyyy / 4, float):
+                mm += 1
+                dd - 28
+
 
     def write_txt(self):
         txt = open(self.file_location + '/hours.txt', "w+")
