bd0df3fa0cb780c81d22e97105549acbac09ccb0
Neeemoo <33062961+Neeemoo@users.noreply.github.com>
2019-09-03 19:24:05 +0200

    progress - 190903-19.24
    changing my program;
    added new lines to the txt file;

--- write_file.py
+++ write_file.py
@@ -1,61 +1,23 @@
 import os
 
-k
+
 class WriteFile:
     file_location = 'C:/Users/' + os.getlogin() + '/.PyHourRecorder'
+    yyyy = 1111
+    mm = 1
+    dd = 1
 
-    time_work_start = []
-    time_lunch_start = []
-    time_lunch_end = []
-    time_work_end = []
-    Day_of_week = ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"]
-    Date_dd_mm_yyyy = [dd.mm.yyyy, dd + 1, mm, yyyy, dd + 2, mm, yyyy, dd + 3, mm, yyyy, dd + 4, mm, yyyy, dd + 5, mm, yyyy, dd + 6, mm, yyyy, dd + 7, mm, yyyy, dd + 8, mm, yyyy, dd + 9, mm, yyyy, dd + 10, mm, yyyy]
+    time_work_start = '00:00'
+    time_lunch_start = '00:00'
+    time_lunch_end = '00:00'
+    time_work_end = '00:00'
+    different_week_days = ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"]
+    date = dd, '.', mm, '.', yyyy
+    std_balance = '00:00'
+    day_of_week = different_week_days[0]
+    std_balance_today = '00:00'
+    std_balance_total = '00:00'
 
-    first_file_print = "+-----------------------------------+ \n" \
-                       "| Start:  | " + Day[0] + ", " + Date[0] + " | " + time_work_start[0] + " | \n" \
-                       "| Mittag: | --------------- | " + time_lunch_start[0] + " | \n" \
-                       "| M-Ende: | --------------- | " + time_lunch_ende[0] + " | \n" \
-                       "| Ende:   | --------------- | " + time_work_ende[0] + " | \n" \
-                       "+-----------------------------------+ "
-
-    def validate_date(self):
-        yyyy = 1111
-        mm = 1
-        dd = 1
-        if mm == 1 or 3 or 5 or 7:
-            if dd > 31:
-                mm += 1
-                dd - 31
-
-        elif mm == 8 or 10 or 12:
-            if dd > 31:
-                mm += 1
-                dd - 31
-
-            if mm == 12:
-                if dd > 31:
-                    mm = 1
-                    dd - 31
-
-        elif mm == 4 or 6 or 9 or 11:
-            if dd > 30:
-                mm += 1
-                dd - 30
-
-        elif mm == 2:
-            if dd > 29 and yyyy == isinstance(yyyy / 4, int):
-                mm += 1
-                dd - 29
-
-            if dd > 28 and yyyy == isinstance(yyyy / 4, float):
-                mm += 1
-                dd - 28
-
-        if dd < 10:
-            dd = "0" + dd
-
-        if mm < 10:
-            mm = "0" + mm
 
     def write_txt(self):
         txt = open(self.file_location + '/hours.txt', "w+")
@@ -64,3 +26,76 @@ class WriteFile:
     def first_write(self):
         first_txt = open(self.file_location + '/hours.txt', "w+")
         first_txt.write(self.first_file_print)
+
+    def print_file(self):
+        file_print = "+-----------------------------------+ \n" \
+                     "| Start:  | --------------- | " + self.time_work_start + " | \n" \
+                     "| Mittag: | --------------- | " + self.time_lunch_start + " | \n" \
+                     "| M-Ende: | --------------- | " + self.time_lunch_end + " | \n" \
+                     "| Ende:   | --------------- | " + self.time_work_end + " | \n" \
+                     "+-----------------------------------+ \n" \
+                     "| Stundenbilanz:  |-----------------| \n" \
+                     "| Heutige-bilanz: | ------- | " + self.std_balance_today + " | \n" \
+                     "| Totale-bilanz:  | ------- | " + self.std_balance_total + " | \n" \
+                     "+-----------------------------------+"
+
+        hour_txt = open(self.file_location + '/hours.txt', "w+")
+        hour_txt.write(str(file_print))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def validate_date(self):
+        if self.mm == 1 or 3 or 5 or 7:
+            if self.dd > 31:
+                self.mm += 1
+                self.dd - 31
+
+        elif self.mm == 8 or 10 or 12:
+            if self.dd > 31:
+                self.mm += 1
+                self.dd - 31
+
+            if self.mm == 12:
+                if self.dd > 31:
+                    self.mm = 1
+                    self.dd - 31
+
+        elif self.mm == 4 or 6 or 9 or 11:
+            if self.dd > 30:
+                self.mm += 1
+                self.dd - 30
+
+        elif self.mm == 2:
+            if self.dd > 29 and self.yyyy == isinstance(self.yyyy / 4, int):
+                self.mm += 1
+                self.dd - 29
+
+            if self.dd > 28 and self.yyyy == isinstance(self.yyyy / 4, float):
+                self.mm += 1
+                self.dd - 28
+
+        if self.dd < 10:
+            self.dd = "0" + self.dd
+
+        if self.mm < 10:
+            self.mm = "0" + self.mm
+
+        if self.dd == 0:
+            self.dd = 1
+
+        return [dd, mm, yyyy]
+
